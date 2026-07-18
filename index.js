f8f146a1b5a6a0170b0085a08c48865e1ab09f84
ibrahim <algedawy89@gmail.com>
2026-07-18 03:27:58 +0300

    Fix deployment_id and assets routing5

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
-import manifestJSON from "__STATIC_CONTENT_MANIFEST";
-const assetManifest = JSON.parse(manifestJSON);
+// index.js
 
 export default {
-  async fetch(request, env, ctx) {
-    try {
-      // Add logic to decide whether to serve an asset or run your original Worker code
-      return await getAssetFromKV(
-        {
-          request,
-          waitUntil: ctx.waitUntil.bind(ctx),
-        },
-        {
-          ASSET_NAMESPACE: env.__STATIC_CONTENT,
-          ASSET_MANIFEST: assetManifest,
-        },
-      );
-    } catch (e) {
-      let pathname = new URL(request.url).pathname;
-      return new Response(`"${pathname}" not found`, {
-        status: 404,
-        statusText: "not found",
+  async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname.startsWith("/api/")) {
+      return new Response(JSON.stringify({ name: "Cloudflare" }), {
+        headers: { "Content-Type": "application/json" },
       });
     }
+
+    return env.ASSETS.fetch(request);
   },
 };
